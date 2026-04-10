188bfa4de8ebf5583f5b211f6ab858b0eee1db81
Lakshya Parashar <lakshyaparashar18@gmail.com>
2026-04-10 20:20:10 +0530

    Branding & Session Final: Established persistent first-party cookies for production

--- api/index.mjs
+++ api/index.mjs
@@ -56,7 +56,7 @@ app.use(
       maxAge: 14 * 24 * 60 * 60 * 1000,
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax'
+      sameSite: 'lax'
     }
   })
 );
